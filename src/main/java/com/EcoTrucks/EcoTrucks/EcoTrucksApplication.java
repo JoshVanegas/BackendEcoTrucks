af5124cd45f845f0d8b7bef89b743971c7a95ab9
Josh Vanegas <vanegasmejiajosepharmando3@gmail.com>
2025-08-25 19:17:51 -0500

    1ra carga de proyecto

---
+++ src/main/java/com/EcoTrucks/EcoTrucks/EcoTrucksApplication.java
@@ -0,0 +1,13 @@
+package com.EcoTrucks.EcoTrucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTrucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTrucksApplication.class, args);
+	}
+
+}
